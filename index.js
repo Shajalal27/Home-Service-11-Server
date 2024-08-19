4e125f6dea179843584809a57f5c19c1fc78701e
Shajalal27 <hpshajalal@gmail.com>
2024-08-19 11:48:16 +0600

    update section

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion } = require('mongodb')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000
 
@@ -27,11 +28,11 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-
+    
     const servicesCollection = client.db('HomeService').collection('populerService')
     const booksCollection = client.db('HomeService').collection('book')
 
-    //auth related api
+  //auth related api
    app.post('/jwt', async(req, res) =>{
     const user = req.body;
     console.log(user)
@@ -136,14 +137,21 @@ async function run() {
       res.send(result)
     })
 
-
-
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    
   }
 }
 run().catch(console.dir);
+
+app.get('/', (req, res) =>{
+    res.send('Hello from server is running')
+})
+
+app.listen(port, () => console.log(`Server runing on port ${port}`))
+
+
+
